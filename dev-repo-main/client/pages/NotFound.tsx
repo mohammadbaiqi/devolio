5cadfe8a02453c9dc2039ecf4ff9b58c4af30bca
Fadli Ismail <fadli@smart-solutions.co.id>
2026-06-27 23:03:26 +0700

    test git

--- dev-repo-main/client/pages/NotFound.tsx
+++ dev-repo-main/client/pages/NotFound.tsx
@@ -15,7 +15,7 @@ const NotFound = () => {
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div className="text-center">
         <h1 className="text-4xl font-bold mb-4">600
-      
+        test git conflict | perubahan dari fadli
         </h1>
         <p className="text-xl text-gray-600 mb-4">Oops! Page not found</p>
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
