c074c2453a42e08d9d145ab8c9da4f856ea72db6
ReiXra <mohammadbaiqi@gmail.com>
2026-07-04 20:50:42 +0700

    fix(-): api build netlify problem

--- dev-repo-main/server/utils/loadDataJson.ts
+++ dev-repo-main/server/utils/loadDataJson.ts
@@ -2,22 +2,16 @@ import { Request, Response } from "express";
 import path from "path";
 import fs from "fs/promises";
 
+const __dirname = import.meta.dirname;
+
 export const loadDataJson = async (name: string) => {
-    const filePath = path.resolve(
-        process.cwd(),
-        "server",
+    const filePath = path.join(
+        __dirname,
         "data",
         name,
         "data.json"
     );
 
-    try {
-        const file = await fs.readFile(filePath, "utf-8");
-        const data = JSON.parse(file);
-
-        return data;
-    } catch (err) {
-        console.error("ERROR loadDataJson:", err);
-        throw err;
-    }
-}
+    const file = await fs.readFile(filePath, "utf-8");
+    return JSON.parse(file);
+};
