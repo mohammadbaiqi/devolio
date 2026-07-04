777ce259e4f2aa4d6590867f25671544d5391f98
ReiXra <mohammadbaiqi@gmail.com>
2026-07-04 21:05:22 +0700

    fix(-): penyesuaian file path api

--- dev-repo-main/server/utils/loadDataJson.ts
+++ dev-repo-main/server/utils/loadDataJson.ts
@@ -1,17 +1,12 @@
-import { Request, Response } from "express";
+import { fileURLToPath } from "url";
 import path from "path";
 import fs from "fs/promises";
 
-const __dirname = import.meta.dirname;
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 export const loadDataJson = async (name: string) => {
-    const filePath = path.join(
-        __dirname,
-        "data",
-        name,
-        "data.json"
-    );
-
+    const filePath = path.join(__dirname, "data", name, "data.json");
     const file = await fs.readFile(filePath, "utf-8");
     return JSON.parse(file);
 };
