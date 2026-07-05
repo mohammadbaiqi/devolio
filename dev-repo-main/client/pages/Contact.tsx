1b37c181d9d7c3ef351a83c8f38f95b7745cbe7c
unknown <khaerulannam16@gmail.com>
2026-07-06 00:20:55 +0700

    ganti warna text input client jadi hitam,tambah pilihan negara,perubahan get in touch dan tombol button ke send message

--- dev-repo-main/client/pages/Contact.tsx
+++ dev-repo-main/client/pages/Contact.tsx
@@ -17,33 +17,33 @@ export default function Contact() {
             
             
             <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-4xl font-semibold tracking-tight text-balance text-[#FFB000] sm:text-5xl">Contact sales</h2>
-              <p className="mt-2 text-lg/8 text-gray-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
+              <h2 className="text-4xl font-semibold tracking-tight text-balance text-[#FFB000] sm:text-5xl">Get in Touch</h2>
+              <p className="mt-2 text-lg/8 text-white-600">Aute magna irure deserunt veniam aliqua magna enim voluptate.</p>
             </div>
             <form action="#" method="POST" className="mx-auto mt-16 max-w-xl sm:mt-20">
               <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
                 <div>
                   <label  className="block text-sm/6 font-semibold text-[#FFB000]">First name</label>
                   <div className="mt-2.5">
-                    <input id="first-name" type="text" name="first-name"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#FFB000] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    <input id="first-name" type="text" name="first-name"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
                   </div>
                 </div>
                 <div>
                   <label className="block text-sm/6 font-semibold text-[#FFB000]">Last name</label>
                   <div className="mt-2.5">
-                    <input id="last-name" type="text" name="last-name"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#FFB000] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    <input id="last-name" type="text" name="last-name"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
                   <label  className="block text-sm/6 font-semibold text-[#FFB000]">Company</label>
                   <div className="mt-2.5">
-                    <input id="company" type="text" name="company"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#FFB000] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    <input id="company" type="text" name="company"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
                   <label  className="block text-sm/6 font-semibold text-[#FFB000]">Email</label>
                   <div className="mt-2.5">
-                    <input id="email" type="email" name="email"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#FFB000] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
+                    <input id="email" type="email" name="email"  className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600" />
                   </div>
                 </div>
                 <div className="sm:col-span-2">
@@ -53,21 +53,25 @@ export default function Contact() {
                       <div className="grid shrink-0 grid-cols-1 focus-within:relative">
                         <select id="country" name="country"  aria-label="Country" className="col-start-1 row-start-1 w-full appearance-none rounded-md py-2 pr-7 pl-3.5 text-base text-gray-500 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6">
                           <option>US</option>
+                          <option>IDN</option>
                           <option>CA</option>
                           <option>EU</option>
+                          <option>UK</option>
+                          <option>AU</option>
+                          <option>IN</option>
                         </select>
                         <svg viewBox="0 0 16 16" fill="currentColor" data-slot="icon" aria-hidden="true" className="pointer-events-none col-start-1 row-start-1 mr-2 size-5 self-center justify-self-end text-gray-500 sm:size-4">
                           <path d="M4.22 6.22a.75.75 0 0 1 1.06 0L8 8.94l2.72-2.72a.75.75 0 1 1 1.06 1.06l-3.25 3.25a.75.75 0 0 1-1.06 0L4.22 7.28a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" fill-rule="evenodd" />
                         </svg>
                       </div>
-                      <input id="phone-number" type="text" name="phone-number" placeholder="123-456-7890" className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-[#FFB000] placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
+                      <input id="phone-number" type="text" name="phone-number" placeholder="123-456-7890" className="block min-w-0 grow py-1.5 pr-3 pl-1 text-base text-black placeholder:text-gray-400 focus:outline-none sm:text-sm/6" />
                     </div>
                   </div>
                 </div>
                 <div className="sm:col-span-2">
                   <label  className="block text-sm/6 font-semibold text-[#FFB000]">Message</label>
                   <div className="mt-2.5">
-                    <textarea id="message" name="message"    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-[#FFB000] outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
+                    <textarea id="message" name="message"    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-black outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600"></textarea>
                   </div>
                 </div>
                 <div className="flex gap-x-4 sm:col-span-2">
@@ -84,7 +88,7 @@ export default function Contact() {
                 </div>
               </div>
               <div className="mt-10">
-                <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Let's talk</button>
+                <button type="submit" className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Send Message</button>
               </div>
             </form>
           </div>
